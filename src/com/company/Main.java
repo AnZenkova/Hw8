dce37a7ef73887ca6f110eaee3111d17ac1ec470
AnZenkova <nastyonka16@mail.ru>
2022-01-21 18:02:26 +1000

    Первое задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,16 @@ package com.company;
 
 public class Main {
 
+    public static void checkYearForLeap(int year){
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        int year = 2000;
+        checkYearForLeap(year);
     }
 }
