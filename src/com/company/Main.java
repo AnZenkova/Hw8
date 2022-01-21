1db3b0f1abd48be6c23cf1b4da22ad52342ce2d6
AnZenkova <nastyonka16@mail.ru>
2022-01-21 18:40:17 +1000

    Второе задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void checkYearForLeap(int year){
@@ -10,8 +12,24 @@ public class Main {
         }
     }
 
+    public static void operatingSystemDefinition (int system, int year){
+        if (system == 0 && year >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (system == 0 && year < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (system != 0 && year >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
     public static void main(String[] args) {
         int year = 2000;
         checkYearForLeap(year);
+
+        int clientOS = 1;
+        int clientDeviceYear = LocalDate.now().getYear();
+        operatingSystemDefinition(clientOS, clientDeviceYear);
     }
 }
